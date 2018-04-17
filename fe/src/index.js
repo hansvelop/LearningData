e3a99ff9decc5c8320a44b3b68a520db72505040
LAPTOP-P8ULP2BT\rexgen <hsw1824@naver.com>
2018-04-17 14:30:57 +0900

    hot loader  적용

--- fe/src/index.js
+++ fe/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {AppContainer} from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootEl = document.getElementById('root');
+ReactDOM.render(
+    <AppContainer>
+        <App />
+    </AppContainer>
+    , rootEl
+);
+
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        const NextApp = require('./App').default; // eslint-disable-line global-require
+        ReactDOM.render(
+        <AppContainer>
+            <NextApp />
+        </AppContainer>,
+        rootEl
+    );
+    });
+}
