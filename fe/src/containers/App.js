c0d4d8f13fcfa8d919f7cb9b6d63355e482365c4
LAPTOP-P8ULP2BT\rexgen <hsw1824@naver.com>
2018-04-17 15:17:53 +0900

    react router 적용

--- fe/src/containers/App.js
+++ fe/src/containers/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
-import '../styles/App.css';
+import '../styles/main/dashboard.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from './admin/Dashboard'
+import Login from './admin/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+          <div>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/login" component={Login} />
+          </div>
+      </Router>
     );
   }
 }
