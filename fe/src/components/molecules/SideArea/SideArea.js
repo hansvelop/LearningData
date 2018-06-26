79b7f3708548a5d5de8d89ed8fc17456d85a2d03
hansvelop <hsw1824@gmail.com>
2018-06-26 13:01:32 +0900

    side Area 수정

--- fe/src/components/molecules/SideArea/SideArea.js
+++ fe/src/components/molecules/SideArea/SideArea.js
@@ -15,10 +15,19 @@ class SideArea extends Component {
                     <Feed.Label className={ cx('icon') }>
                         <img src={iconImg}/>
                     </Feed.Label>
-                    <Icon name={'plus'} size={'big'} />
+                    <Icon name={'plus'} size={'large'} className={cx('add-icon')} />
                 </div>
                 <div className={cx('sub')}>
                     <p className={cx('title')}>Healthy</p>
+                    <div className={cx('menu-area')}>
+                        <ul>
+                            <li><Icon name={'file audio outline'} size={'big'} className={cx('select')}/> Your work</li>
+                            <li><Icon name={'file audio outline'} size={'big'} /> Your work</li>
+                            <li><Icon name={'file audio outline'} size={'big'} /> Your work</li>
+                            <li><Icon name={'file audio outline'} size={'big'} /> Your work</li>
+                            <li><Icon name={'file audio outline'} size={'big'} /> Your work</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         );
